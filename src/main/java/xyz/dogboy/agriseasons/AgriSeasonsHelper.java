ac16a0a511cbdfbe0649b611d2dadfcbbf1e6de3
Dogboy21 <dogboy2103@gmail.com>
2019-03-23 18:00:17 +0100

    Improved detection of AgriCraft plantables
    This "should" fix the missing seasonal effect on a few plants
    
    If there's a seed which can't be associated with it's correlating plant,
    a log message is shown with detailed seed information. Try tweaking the
    AgriCraft config and test again, otherwise report in the issue tracker.

--- src/main/java/xyz/dogboy/agriseasons/AgriSeasonsHelper.java
+++ src/main/java/xyz/dogboy/agriseasons/AgriSeasonsHelper.java
@@ -1,18 +1,27 @@
 package xyz.dogboy.agriseasons;
 
+import com.infinityraider.agricraft.api.v1.plant.IAgriPlant;
+import com.infinityraider.agricraft.api.v1.util.FuzzyStack;
 import com.infinityraider.agricraft.tiles.TileEntityCrop;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockGrass;
-import net.minecraft.block.BlockReed;
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemBlockSpecial;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.IPlantable;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import sereneseasons.api.SSBlocks;
 import sereneseasons.config.FertilityConfig;
 import sereneseasons.init.ModFertility;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class AgriSeasonsHelper {
+    private static final Logger log = LogManager.getLogger("AgriSeasons");
+    private static final Set<String> missingPlants = new HashSet<>();
 
     private static boolean isGreenhouseGlassAboveBlock(World world, BlockPos cropPos) {
         for(int i = 0; i < FertilityConfig.general_category.greenhouse_glass_max_height; ++i) {
@@ -29,17 +38,30 @@ public class AgriSeasonsHelper {
             return null;
         }
 
-        Item item = crop.getSeed().getPlant().getSeed().getItem();
-        if (item instanceof IPlantable) {
-            return (IPlantable) item;
-        }
+        return crop.getSeed().getPlant().getSeedItems().stream()
+                .map(FuzzyStack::toStack)
+                .map(ItemStack::getItem)
+                .map(item -> {
+                    if (item instanceof ItemBlock) {
+                        return ((ItemBlock) item).getBlock();
+                    } else if (item instanceof ItemBlockSpecial) {
+                        return ((ItemBlockSpecial) item).getBlock();
+                    }
 
-        return null;
+                    return item;
+                })
+                .filter(object -> object instanceof IPlantable)
+                .map(item -> (IPlantable) item)
+                .findFirst()
+                .orElse(null);
     }
 
     public static boolean onGrowthTickPre(TileEntityCrop crop) {
         IPlantable plantable = AgriSeasonsHelper.getPlantable(crop);
         if (plantable == null) {
+            if (crop.getSeed() != null) {
+                AgriSeasonsHelper.printMissingPlantable(crop.getSeed().getPlant());
+            }
             return false;
         }
 
@@ -47,7 +69,7 @@ public class AgriSeasonsHelper {
 
         boolean isFertile = ModFertility.isCropFertile(block.getRegistryName().toString(), crop.getCropWorld(), crop.getCropPos());
         if (FertilityConfig.general_category.seasonal_crops && !isFertile && !AgriSeasonsHelper.isGreenhouseGlassAboveBlock(crop.getCropWorld(), crop.getCropPos())) {
-            if (FertilityConfig.general_category.crops_break && !(block instanceof BlockGrass) && !(block instanceof BlockReed)) {
+            if (FertilityConfig.general_category.crops_break) {
                 crop.getCropWorld().destroyBlock(crop.getCropPos(), true);
             }
 
@@ -55,6 +77,20 @@ public class AgriSeasonsHelper {
         }
 
         return false;
+    }
+
+    private static void printMissingPlantable(IAgriPlant plant) {
+        if (!AgriSeasonsHelper.missingPlants.add(plant.getId())) {
+            return;
+        }
+
+        AgriSeasonsHelper.log.warn("Unable to find valid plantable for AgriCraft plant " + plant + ". Is it misconfigured?");
+        AgriSeasonsHelper.log.warn("Plant identification:");
+        AgriSeasonsHelper.log.warn("  ID: " + plant.getId());
+        AgriSeasonsHelper.log.warn("  Plant Name: " + plant.getPlantName());
+        AgriSeasonsHelper.log.warn("  Seed Name: " + plant.getSeedName());
+        AgriSeasonsHelper.log.warn("  Seed Items:");
+        plant.getSeedItems().forEach(stack -> AgriSeasonsHelper.log.warn("    - " + stack.toString() + " (" + stack.toStack().getItem().getClass().getCanonicalName() + ")"));
     }
 
 }
